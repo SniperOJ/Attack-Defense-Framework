3ec87c7fcb8ca406696cdbcfd5f123f8dc2cfdaf
Wang Yihang <wangyihanger@gmail.com>
2018-11-22 01:30:56 +0800

    [+] Add generator.py for chat

---
+++ services/Rocket.Chat/generator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import configparser
+import os
+import sys
+
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+
+suffix = "lilac.com"
+service = "chat"
+
+data = open("docker-compose.yml.example").read()
+data = data.replace("__HOST__", config.get(service.lower(), "host"))
+data = data.replace("__PORT__", config.get(service.lower(), "port"))
+with open("docker-compose.yml", "w") as f:
+    f.write(data)
