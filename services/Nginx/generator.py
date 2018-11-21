b732b572518060764d2b3eaf3a0a26fc115fd7ba
Wang Yihang <wangyihanger@gmail.com>
2018-11-21 21:12:58 +0800

    [+] Update Nginx

--- services/Nginx/generator.py
+++ services/Nginx/generator.py
@@ -3,16 +3,7 @@
 import configparser
 import os
 
-def generate(service):
-    config = configparser.ConfigParser()
-    config.read('../config.ini')
-    template = open("sites-available/template").read()
-    with open("sites-available/%s" % (service.lower()), "w") as f:
-        data = template
-        data = data.replace("__HOST__", config.get(service.lower(), "host"))
-        data = data.replace("__PORT__", config.get(service.lower(), "port"))
-        data = data.replace("__NAME__", service.lower())
-        f.write(data)
+suffix = "lilac.com"
 
 def auth():
     config = configparser.ConfigParser()
@@ -22,7 +13,34 @@ def auth():
     command = 'printf "%s:$(openssl passwd -crypt %s)\n" > ./auth/basic' % (username, password)
     os.system(command)
 
-generate("sirius")
-generate("submittor")
-generate("platypus")
+def hosts(services):
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+    data = ""
+    for service in services:
+        data += "%s\t%s" % (config.get(service.lower(), "host"), "%s.%s" % (service.lower(), suffix))
+    with open("hosts", "w") as f:
+        f.write(data)
+
+def nginx(services):
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+    template = open("sites-available/template").read()
+    for service in services:
+        with open("sites-available/%s" % (service.lower()), "w") as f:
+            data = template
+            data = data.replace("__HOSTNAME__", "%s.%s" % (service.lower(), suffix))
+            data = data.replace("__HOST__", config.get(service.lower(), "host"))
+            data = data.replace("__PORT__", config.get(service.lower(), "port"))
+            data = data.replace("__NAME__", service.lower())
+            f.write(data)
+
+services = [
+    "sirius",
+    "submittor",
+    "platypus",
+]
+
 auth()
+hosts(services)
+nginx(services)
