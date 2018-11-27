03b9d06c798fbfbad00a45ef180e4dd815e3706c
Wang Yihang <wangyihanger@gmail.com>
2018-11-27 20:50:18 +0800

    [+] 2018-HXB

--- services/Bind9/generator.py
+++ services/Bind9/generator.py
@@ -6,10 +6,9 @@ import sys
 
 host = sys.argv[1]
 data = open("db.lilac.com.example").read()
-data = data.replace("__HOST__", host)
 with open("db.lilac.com", "w") as f:
+    data = data.replace("__HOST__", host)
     f.write(data)
-
 
 os.system("cp db.lilac.com /etc/bind/")
 os.system("cp named.conf.default-zones /etc/bind/")
